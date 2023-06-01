752e8848200682b518c365fa203f0a283dc0ccc4
Paul Ramon <peramon98.18@gmail.com>
2023-05-31 19:53:06 -0500

    Agregando la funcionalidad de encriptar en js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 console.log('Bienvenido desarrollador Backend')
+
+const frase = document.querySelector(".frase");
+const frase_encriptada = document.querySelector(".frase-encriptada");
+/* const copia = document.querySelector(".copiar");
+copia.style.display = "none" */
+
+
+function validarTexto(){
+    let textoEscrito = document.querySelector(".frase").value;
+    let validador = textoEscrito.match(/^[a-z]*$/);
+
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos")
+        location.reload();
+        return true;
+    }
+}
+
+//Laves de encriptacion
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+        }
+
+    }
+    return stringEncriptada
+}
+
+
+
+
